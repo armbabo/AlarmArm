2f26b8ee97e8beb83979991e414261c0984e8496
nguyenloc <locnguyen@teqnological.asia>
2019-01-15 11:34:32 +0700

    edit import

--- app/src/main/java/com/arm/timetable/view/activity/mainActivity/MainActivity.kt
+++ app/src/main/java/com/arm/timetable/view/activity/mainActivity/MainActivity.kt
@@ -8,9 +8,9 @@ import com.arm.timetable.view.fragment.HomeFragment
 import io.reactivex.subjects.PublishSubject
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
-import org.koin.android.scope.ext.android.bindScope
-import org.koin.android.scope.ext.android.getOrCreateScope
-import org.koin.android.scope.ext.android.getScope
+import org.koin.androidx.scope.ext.android.bindScope
+import org.koin.androidx.scope.ext.android.getOrCreateScope
+import org.koin.androidx.scope.ext.android.getScope
 import org.koin.core.parameter.parametersOf
 
 class MainActivity : BaseActivity<MainActivityViewModel>(), MainActivityView {
--- app/src/main/java/com/arm/timetable/view/fragment/alarmFragment/AlarmModule.kt
+++ app/src/main/java/com/arm/timetable/view/fragment/alarmFragment/AlarmModule.kt
@@ -1,6 +1,6 @@
 package com.arm.timetable.view.fragment.alarmFragment
 
-import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val alarmModule = module {
